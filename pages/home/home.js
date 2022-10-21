a9ddd2fda1d64ea5dc6ea8dae841d1fad121e0f4
sandwichfe <1344382941@qq.com>
2022-10-21 17:12:02 +0800

    获取openid
    获取openid

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,7 +4,7 @@ Page({
         list,
     },
     onLoad(options) {
-        const { path, q } = options;
+         const { path, q } = options;
         console.log(path);
         if (q) {
             const str = this.getQueryByUrl(decodeURIComponent(q));
@@ -13,9 +13,39 @@ Page({
                 url: `/pages/${str.page}/${str.page}`,
             });
         }
-    },
+      },
     clickHandle(e) {
         let { name, path = '' } = e.detail.item;
+        // add
+        if(name == 'Button'){
+          // wx.requestSubscribeMessage({
+          //   tmplIds: ['5oP0dFBcEpixmI6tahxCiC1CKvlG2eWXIs0jDO6lu-Y'],
+          //   success (res) { }
+          // })
+          wx.login({
+            //成功放回
+            success:(res)=>{
+              console.log(res);
+              let code=res.code
+              let appid = '22';
+              let srt = '11';
+              wx.request({
+                url: `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${srt}&js_code=${code}&grant_type=authorization_code`,
+                success:(res)=>{
+                  console.log(res);
+                  userInfo.openid=res.data.openid
+                  //获取到你的openid
+                  console.log(userInfo.openid);
+                }
+              })
+          
+            }
+          })
+
+
+
+        }
+        // end
         if (!path) {
             name = name.replace(/^[A-Z]/, (match) => `${match}`.toLocaleLowerCase());
             name = name.replace(/[A-Z]/g, (match) => {
